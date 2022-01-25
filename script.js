e3b1049fcd38401fd7ecd0dcf833e1af7a0bd359
caio-chcn <caioh.mec@gmail.com>
2022-01-25 16:17:33 -0300

    segunda atividade de lógica

---
+++ script.js
@@ -0,0 +1,16 @@
+//1 - Crie uma condição que exiba uma mensagem no console se a idade for maior que 18
+let idade = 30
+if(idade >=18){
+    console.log("Ja é maior de idade e pode ser preso se der mole")
+}
+
+//2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a condição humana seja true
+let humano = true
+if( idade >=18 && humano == true){
+console.log("Você é humano e maior de idade")
+}
+//3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
+let dezJan = true
+if( dezJan == true){
+    console.log( "Você é nasceu em Dez ou em Jan")
+}
